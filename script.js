621626d306638daac6be197a63d425d3af26ec83
Agus27111 <agussetiawan.belajar@gmail.com>
2023-12-27 05:20:28 +0700

    i have finished the large screen

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // for error or success message
 
-//we used oneClick method
+//i have used oneClick method
 function subscribe() {
   const emailValue = document.getElementById("email");
   const errorElement = document.querySelector(".textvalidity");
@@ -14,4 +14,17 @@ function subscribe() {
     card1.classList.add("hidden");
     card2.classList.remove("hidden");
   }
+}
+
+//try to change backround hero with JS
+let value = 1080;
+
+if (window.innerWidth < 500) {
+  document.querySelector("#imgHero").src =
+    "./assets/images/illustration-sign-up-mobile.svg";
+} else if (window.innerWidth >= 800) {
+  document.querySelector("#imgHero").src =
+    "./assets/images/illustration-sign-up-desktop.svg";
+} else {
+  console.log(window.innerWidth);
 }
